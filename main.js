e486f5308fd5ce1978eace6305b44f8a11c75741
iregretnothing <athlon860k@yandex.ru>
2017-04-27 16:27:10 +0300

    members.js

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 var redis = require('redis');
 var VK = require('vk-io');
-var Members = require("./members.js");
+var Members = require("./members_2.js");
 
 client = redis.createClient();
 
 var vk = new VK({});
 
 vk.setOptions({
-    app: 0,
+    app: 6005112,
     login: 'leoweyfreelancewallet@yandex.ru',
-    pass: '',
-    phone: ''
+    pass: 'blitz3dfastextmangoworms12345',
+    phone: '+79683373481'
 });
 
-vk.setToken("");
+
+
+
+
+
+vk.setToken("41730f1cc6e1cc35b845f23a505f1e8053eb1800c836632539284521584288f0f76dbcbe077772297611e");
 
 var members = new Members(vk, client, "vaporlab");
 
@@ -21,3 +26,4 @@ members.getAllMembers(function(){console.log("wew")});
  //members.isMember(858, function(res){console.log(res)});
 
 // members.delId(2363459);
+
--- members.js
+++ members.js
@@ -7,50 +7,43 @@ module.exports = members = function(vk, client, group_id) {
 
 members.prototype.getAllMembers = function(callback) {
 
-	var vk = this.vk;
-	var client = this.client;
-	var group_id = this.group_id;
-
-	var step = 1000;
+	const vk = this.vk;
+	const client = this.client;
+	const group_id = this.group_id;
 
 	client.del(group_id, function() {
-		stepGet(group_id, step, 0, callback);
-	});
 
-	
-	function stepGet(group_id, step, i, callback) {
-		vk.api.groups.getMembers({
-	            group_id: group_id,
-	            count: step,
-	            offset: i
-	        })
-	        .then( function(members) {
-	        	client.lpush(group_id, members.items);
-	        	i += step;
-	        	if(i<members.count) {
-	        		stepGet(group_id, step, i, callback);
-	        	} else {
-	        		callback();
-	        	}
-	        })
-	        .catch( function(error) {
-	            console.error(error);
-	    });
-	}
+		vk.collect.groups.getMembers({
+			group_id: group_id
+		})
+		.then(function(members) {
+			client.lpush(group_id, members, function() {
+				callback();
+			});
+		})
+		.catch(function(error) {
+			console.error(error);
+		});
+
+	});
 
 }
 
 members.prototype.isMember = function(user_id, callback) {	
 
-	var client = this.client;
-	var group_id = this.group_id;
+	const client = this.client;
+	const group_id = this.group_id;
 
 	client.lrange(group_id, 0, -1, function(err, reply) {
-		if(err) {callback(err)}
+
+		if(err) {
+			callback(err)
+		}
+
 		reply.forEach(function(value, i) {
 			if(value == user_id) {
 				callback(i);
-			};
+			}
 		});
 
 	});
