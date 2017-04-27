3b778870fe0722ba4e137958444a17a6ef131f43
iregretnothing <athlon860k@yandex.ru>
2017-04-27 16:29:04 +0300

    main.js

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ var vk = new VK({});
 
 vk.setOptions({
     app: 6005112,
-    login: 'leoweyfreelancewallet@yandex.ru',
-    pass: 'blitz3dfastextmangoworms12345',
-    phone: '+79683373481'
+    login: '',
+    pass: '',
+    phone: ''
 });
 
 
@@ -18,7 +18,7 @@ vk.setOptions({
 
 
 
-vk.setToken("41730f1cc6e1cc35b845f23a505f1e8053eb1800c836632539284521584288f0f76dbcbe077772297611e");
+vk.setToken("");
 
 var members = new Members(vk, client, "vaporlab");
 
